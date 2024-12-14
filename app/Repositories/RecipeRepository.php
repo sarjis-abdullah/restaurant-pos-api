0c599378babed6440f99f0cf9b145131f48f999d
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-12-14 19:20:46 +0600

    recipe added

---
+++ app/Repositories/RecipeRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repositories;
+use App\Repositories\Contracts\{RecipeInterface};
+
+use App\Models\Recipe;
+
+class RecipeRepository extends BaseRepository implements RecipeInterface
+{
+
+}
---
+++ database/migrations/2024_12_14_191609_create_recipes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('category')->nullable(); //Recipe category (e.g., Fast Food, Dessert).
+            $table->integer('preparation_time')->nullable(); //Estimated preparation time (in minutes).
+            $table->integer('serves')->nullable(); //Number of servings this recipe produces.
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
